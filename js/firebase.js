b205dc49846c65e559bfeced3a8728082f24a100
Tejas-Choudekar <tejus.choudekar@gmail.com>
2019-04-24 01:30:28 +0530

    Products page made dynamic and some css changes are to be made

--- js/firebase.js
+++ js/firebase.js
@@ -9,7 +9,6 @@
   };
   firebase.initializeApp(config);
   var db = firebase.firestore();
-
   function fetchImage(productId) {
     var storageRef = firebase.storage().ref();
     storageRef.child(productId).getDownloadURL().then(function (url) {
@@ -25,24 +24,21 @@
 
   function fetchProduct() {
     console.log("called");
-    var productId = "fn7gKULPpMf9vsCb1XMR";
+    var productId = sessionStorage.getItem("productId");
     console.log(productId);
     var docRef = db.collection("products").doc(productId);
     docRef.get().then(function (doc) {
-
-      if (doc.exists) {
+       if (doc.exists) {
         console.log("Product data:", doc.data());
         var storageRef = firebase.storage().ref();
         storageRef.child(productId).getDownloadURL().then(function (url) {
           var test = url;
-          alert(url);
           document.querySelector('#proImg').src = test;
         })
         var display = '<section id="content"><div class="container"><div class="row"><div class="span8"><article><div class="top-wrapper"><div class="post-heading"><h3><a href="#">' + doc.data().name + '</a></h3></div><div class="flexslider"><ul class="slides"><li class="flex-active-slide" data-thumb-alt="" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;"><img id="proImg" src="" alt="" draggable="false"></li><li data-thumb-alt="" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;"><img id="proImg" src="" alt="" draggable="false"></li><li data-thumb-alt="" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;"><img id="proImg" src="" alt="" draggable="false"></li></ul><ol class="flex-control-nav flex-control-paging"><li><a href="#" class="flex-active">1</a></li><li><a href="#">2</a></li><li><a href="#">3</a></li></ol><ul class="flex-direction-nav"><li class="flex-nav-prev"><a class="flex-prev" href="#">Previous</a></li><li class="flex-nav-next"><a class="flex-next" href="#">Next</a></li></ul></div></div><p>' + doc.data().description + '</p></article></div><div class="span4"><aside class="right-sidebar"><div class="widget"><h5 class="widgetheading">Project information</h5><ul class="folio-detail"><li><label>Category :</label> ' + doc.data().category + '</li><li><label>Brand :</label> ' + doc.data().brand + '</li><li><label>Price :</label> ' + doc.data().price + '</li><li><label>Items left :</label>' + doc.data().quantity + '</li></ul></div><div class="widget"><h5 class="widgetheading">Text widget</h5><p>' + doc.data().specification + '</p></div></aside></div></div></div></section>';
         document.getElementById('productDetails').innerHTML = display;
 
       } else {
-        // doc.data() will be undefined in this case
         console.log("No such product!");
       }
     }).catch(function (error) {
@@ -53,34 +49,22 @@
 
   function fetchProducts() {
     db.collection("products").get().then(function (querySnapshot) {
-      //var array = Array();
       var productList = [];
       var count = 0;
-
       querySnapshot.forEach(function (doc) {
         var imgId = doc.id;
-
         console.log(doc.id);
-
-
         var storageRef = firebase.storage().ref();
         storageRef.child(doc.id).getDownloadURL().then(function (url) {
           console.log(url);
-          document.querySelector("#" + imgId).src = url;
-          //document.querySelector("#"+imgCard).href = url;
+          document.querySelector("#" + imgId).src = url;  
         });
         var Test = doc.id;
-
-        productList += '<li onclick="productDetail('+imgId+')" class="item-thumbs span3 design" data-id="id-0" data-type="web"><a class="hover-wrap" data-fancybox-group="gallery" title="' + doc.data().name + '"><span class="overlay-img"></span><span class="overlay-img-thumb font-icon-plus"></span></a><img id="' + imgId + '" alt="' + doc.data().description + '"></li>';
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+        productList += '<li onclick="productDetail('+imgId+')" class="item-thumbs span3 design col-md-4 col-xs-6" data-id="id-0" data-type="'+doc.data().category+'"><a class="hover-wrap" data-fancybox-group="gallery" title="' + doc.data().name + '"><span class="overlay-img"></span><span class="overlay-img-thumb font-icon-plus"></span></a><img id="' + imgId + '" alt="' + doc.data().description + '"><div class="product-body"><h6 class="product-category">Category: '+doc.data().category+'</h6><h6 class="product-name">Name: '+doc.data().name+'</h6><h6 class="product-price">Price: ₹'+doc.data().price+'</h6></div></li>';
+        console.log(doc.id, " => ", doc.data()); 
         count++
-        //return productList;
       });
-      // Caling storeInSession method onclick is giving error. Stuck badly here!
-
       document.getElementById('productAppender').innerHTML = productList;
-
     }).catch(function (error) {
       console.log("Error getting Products:", error);
     });
@@ -93,11 +77,12 @@ function productDetail(product){
   location.href = "portfolio-detail.html";
 }
 
+function sorter(){
+  sessionStorage.setItem("sorting", product.id);
+}
 
-  document.getElementById("imageId").addEventListener("click", function storeInSession(event){
-    console.log(this);
-    //var item = doc.id;
-    
+ document.getElementById("imageId").addEventListener("click", function storeInSession(event){
+ console.log(this);   
 });
 
   function uploadImage(productId) {
@@ -118,8 +103,6 @@ function productDetail(product){
         quantity: $('#productQuantity').val(),
         brand: $('#productBrand').val(),
         specification: $('#productSpecification').val(),
-
-
       })
       .then(function (docRef) {
         console.log("Document written with ID: ", docRef.id);
