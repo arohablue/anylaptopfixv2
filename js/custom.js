18639ef8eb73796877ee9e20c97603c1e073055b
Tejas-Choudekar <tejus.choudekar@gmail.com>
2019-05-05 17:12:39 +0530

    call us button is functional, nav bar has been updated

--- js/custom.js
+++ js/custom.js
@@ -3,36 +3,37 @@ jQuery(document).ready(function($) {
   "use strict";
 
 
-  // (function() {
+  (function() {
 
-  //   var $menu = $('.navigation nav'),
-  //     optionsList = '<option value="" selected>Go to..</option>';
+    var $menu = $('.navigation nav'),
+      optionsList;// = '<option value="" selected>Go to..</option>';
 
-  //   $menu.find('li').each(function() {
-  //       var $this = $(this),
-  //         $anchor = $this.children('a'),
-  //         depth = $this.parents('ul').length - 1,
-  //         indent = '';
+    $menu.find('li').each(function() {
+        var $this = $(this),
+          $anchor = $this.children('a'),
+          depth = $this.parents('ul').length - 1,
+          indent = '',
+          spacing = '';
 
-  //       if (depth) {
-  //         while (depth > 0) {
-  //           indent += ' - ';
-  //           depth--;
-  //         }
+        if (depth) {
+          while (depth > 0) {
+            indent = '15px';
+            spacing +=' - '
+            depth--;
+          }
 
-  //       }
-  //       $(".nav li").parent().addClass("bold");
+        }
+        $(".nav li").parent().addClass("bold");
 
-  //       //optionsList += '<option value="' + $anchor.attr('href') + '">' + indent + ' ' + $anchor.text() + '</option>';
-  //       optionsList +='<li><a href="' + $anchor.attr('href') + '">' + indent + ' ' + $anchor.text() +'</a></li>';
-  //     }).end()
-  //     .after('<ul class="nav topnav">' + optionsList + '</ul>');
+        optionsList += '<option style="font-size:' + indent + '" value="' + $anchor.attr('href') + '">' + spacing +' '+ $anchor.text() + '</option>';
+      }).end()
+      .after('<select class="selectmenu">' + optionsList + '</select>');
 
-  //   $('select.selectmenu').on('change', function() {
-  //     window.location = $(this).val();
-  //   });
+    $('select.selectmenu').on('change', function() {
+      window.location = $(this).val();
+    });
 
-  // })();
+  })();
 
 
   $('.toggle-link').each(function() {
